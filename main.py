c7caa196bc4a5d14fd59231ce333e6922d8b77c5
Merit Student <student@meac.local>
2018-09-14 13:40:03 -0600

    hi

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ while True:
     if fromUser == "start":
         print("START GAME!")
     elif fromUser == "quit": # else if
-        print("Game over!!!!!!!")
+        print("So soon?")
         break # Ends the while loop    
+    else:
+    elif fromUser.split()[0] == go:
+        print("Go somewhere")
+    elif fromUser == "attack":
+        print("Attack a thing")
     else:
         print("To start, type'start'")
     fromUser = input()
